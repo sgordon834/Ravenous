0e79c1d847dd35d222854b4b3adc712e12f1b457
sgordon834 <sgordon834@gmail.com>
2018-05-26 09:11:50 -0700

    done

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -29,8 +29,8 @@ class Business extends React.Component {
     </div>
     <div className="Business-reviews">
       <h3>{business.category}</h3>
-      <h3 className="rating">4.5 stars}</h3>
-      <p>90 reviews</p>
+      <h3 className="rating">{business.rating} stars</h3>
+      <p>{business.reviewCount} reviews</p>
     </div>
   </div>
 </div>
