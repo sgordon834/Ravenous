eb8f6409e9337920677baea2465fe080a9af995e
sgordon834 <sgordon834@gmail.com>
2018-05-26 09:06:51 -0700

    more object lessons

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -18,9 +18,9 @@ class Business extends React.Component {
     return (
       <div name="Business">
   <div className="image-container">
-    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+    <img src={business.imageSrc} alt=" "/>
   </div>
-  <h2>MarginOtto Pizzeria</h2>
+  <h2>{business.name}</h2>
   <div className="Business-information">
     <div className="Business-address">
       <p>{business.address}</p>
@@ -28,7 +28,7 @@ class Business extends React.Component {
       <p>{business.state}</p>
     </div>
     <div className="Business-reviews">
-      <h3>ITALIAN</h3>
+      <h3>{business.category}</h3>
       <h3 className="rating">4.5 stars}</h3>
       <p>90 reviews</p>
     </div>
