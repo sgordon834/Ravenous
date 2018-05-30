041dba3302c20f22616b285ee926d66c84afb2c2
sgordon834 <sgordon834@gmail.com>
2018-05-29 18:32:25 -0700

    finished ravenous 3

--- src/App.js
+++ src/App.js
@@ -25,11 +25,16 @@ const businesses = [
   business
 ];
 class App extends Component {
+
+  searchYelp(term, location, sortBy){
+    console.log(`Search Yelp using ${term}, ${location}, ${sortBy}!`)
+  }
+
   render() {
     return (
       <div className="App">
-  <h1>ravenous</h1>
-  <SearchBar />
+  <h1>ravenouser</h1>
+  <SearchBar searchYelp={this.searchYelp} />
   <BusinessList businesses={businesses} /> 
 </div>
     );
